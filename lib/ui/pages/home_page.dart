3c626b41324169d490343de66015905129e0d745
HASURO <rentaro170928@icloud.com>
2023-12-05 19:28:31 +0900

    add: localization #11

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -3,16 +3,18 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:qiita_app/data/model/post/post_model.dart';
 import 'package:qiita_app/data/provider/post/post_provider.dart';
 import 'package:qiita_app/ui/components/post/post_list_box.dart';
+import 'package:qiita_app/ui/hooks/use_l10n.dart';
 
-class MyHomePage extends ConsumerWidget {
+class MyHomePage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final AsyncValue<List<PostModel>> posts = ref.watch(postNotifierProvider);
+    final l10n = useL10n(context);
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Qiita'),
+        title: Text(l10n.qiita),
       ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.center,
