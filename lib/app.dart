279a2b2d6f6f67b85a9ba1856a076b2e65d648bb
HASURO <rentaro170928@icloud.com>
2023-12-21 19:13:34 +0900

    add: auto route

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:qiita_app/ui/routes/app_router.dart';
 import 'package:qiita_app/ui/theme/colors.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 import 'ui/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _appRouter.config(),
       title: 'Flutter Demo',
       localizationsDelegates: L10n.localizationsDelegates,
       supportedLocales: L10n.supportedLocales,
@@ -17,7 +19,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryGreen),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
     );
   }
 }
